9306248bd66e5bf082b8c6fe421edd6a74d24523
Franco Petosa Ayala <84604362+FrancoPetosa99@users.noreply.github.com>
2023-01-17 08:56:55 -0300

    add cart end points: POST and GET

--- src/controllers/carts.js
+++ src/controllers/carts.js
@@ -1,12 +1,42 @@
 import { Router } from "express";
+import { CartManager } from "../CartManager.js";
 
 const router = Router();
+const cartManager = new CartManager();
 
-//end point GET carts
-router.get('/', async (request, response)=> {
+//end point GET cart by Id
+router.get('/:cid', async (request, response)=> {
+    try{
+        const cartId = request.params.cid;
+        const products = await cartManager.getCartProductsById(cartId);
+        response.json(200, products);
+    }catch(error){
+        response.json(400, 'An error occurred during process:' + error.message);
+    }
+});
 
-    response.json('end point GET carts')
+//end point POST carts
+router.post('/', async (request, response)=> {
+    try{
+        await cartManager.addCart();
+        response.json(200, 'Cart successfully created');
+    }catch(error){
+        response.json(400, 'An error occurred during process:' + error.message);
+    }
+});
 
+//end point POST add product Id to cart by Id
+router.post('/:cid/product/:pid', async (request, response)=> {
+    try{
+        const cartId = request.params.cid;
+        const productId = request.params.pid;
+
+        await cartManager.addProductToCart(productId, cartId);
+
+        response.json(200, 'Cart successfully created');
+    }catch(error){
+        response.json(400, 'An error occurred during process:' + error.message);
+    }
 });
 
 export default router;
