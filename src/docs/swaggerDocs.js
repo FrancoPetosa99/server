bdb31562464f6500697ad64e2c40483b86808f94
Franco Petosa Ayala <84604362+FrancoPetosa99@users.noreply.github.com>
2023-05-21 23:15:07 -0300

    updates

---
+++ src/docs/swaggerDocs.js
@@ -0,0 +1,33 @@
+import swaggerJsDoc from 'swagger-jsdoc';
+import swaggerUiExpress from 'swagger-ui-express';
+import __dirname from '../utils.js';
+
+function Swagger(){
+
+    const options = {
+        definition: {
+            openapi: '3.0.0',
+            info: {
+                title: 'API REST - Server documentation',
+                description: 'Here goes a brief description'
+            }
+        },
+        apis: [`${__dirname}/docs/**/*.yaml`]
+    };
+    console.log(__dirname);
+    const ui = swaggerUiExpress.serve;
+
+    function setUp(){
+        const specs = swaggerJsDoc(options);
+        return swaggerUiExpress.setup(specs);
+    }
+
+    return {
+        setUp,
+        ui
+    }
+}
+
+const swagger = Swagger();
+
+export default swagger;
--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ import __dirname from './utils.js';
 import ServerSocket from './sockets/index.js';
 import passport from 'passport';
 import initializePassport from './config/passport.js';
+import swagger from './docs/swaggerDocs.js';
 
 const app = express();
 
@@ -26,6 +27,7 @@ app.get('/', ( /*request, response*/ )=> console.log(`Node Server running at por
 //MIDDLEWARE CONFIGURATIONS
 //Description: This functions are executed first before the request reaches the endpoint
 /********************************************/
+app.use('/docs', swagger.ui, swagger.setUp());
 app.use(express.json()); //convert data sent on the body request to javascript object
 app.use(cookieParser()); //convert data sent from cookies browser to javascript object
 app.use(express.static(__dirname + '/public'));
@@ -43,8 +45,6 @@ app.use(passport.initialize());
 
 const serverSocket = ServerSocket(httpServer); //INITIALIZE SOCKET SERVER
 
-
-
 /********************************************/
 //ENGINE TEMPLATE CONFIGURATIONS
 //Description: this configurations allow the api to work with engine templates (type: module)
@@ -52,8 +52,6 @@ const serverSocket = ServerSocket(httpServer); //INITIALIZE SOCKET SERVER
 app.engine('handlebars', handlebars.engine());
 app.set('views', __dirname + '/views');
 app.set('view engine', 'handlebars');
-
-
 
 connectMongodb(); //CONNECT SERVER TO MONGODB DATA BASE
 routes(app); //INITIALIZE SERVER ROUTES
